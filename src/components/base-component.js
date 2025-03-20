f77ddd31421deb68a4d1f09bdcbdd0309e5053ed
halex22 <halex2209@gmail.com>
2025-03-20 23:47:03 +0100

    fav page works

--- src/components/base-component.js
+++ src/components/base-component.js
@@ -19,15 +19,18 @@ export default class BaseComponent{
   render() {
     const mainContainer = document.getElementById('main-container')
     mainContainer.innerHTML = ''
+    const favBooks = Array.from(this.storageService.loadSavedBooks() ?? [])
 
     for (const book of this.books) {
+      console.log(book)
       const bookInfo = {
         title: book.title,
         author: book.authors[0].name,
         imgUrl: book.formats['image/jpeg'],
         id: book.id,
+        isFavorite: !!favBooks.find(savedBook => book.id === savedBook.id)
       }
-      const bookCard = new BookCardComponent(bookInfo)
+      const bookCard = new BookCardComponent(bookInfo, this.storageService)
       const card = bookCard.createBookCardHtml()
       mainContainer.appendChild(card)
     }
--- src/save.js
+++ src/save.js
@@ -1,1 +1,12 @@
 import './style.css'
+import SavePageComponent from './components/save-page-components'
+import BookService from './services/book-service'
+import StorageService from './services/storage-service'
+
+const bookService = new BookService()
+const storageService = new StorageService()
+
+
+const saveComponent = new SavePageComponent(bookService, storageService)
+
+saveComponent.start()
