3fcf37635d74e8dfe0bdf4a04b2cec40843b33de
halex22 <halex2209@gmail.com>
2025-03-21 08:48:45 +0100

    commit b4 singleton book service

---
+++ src/services/book-service-singleton.js
@@ -0,0 +1,43 @@
+let instance
+
+
+class BookService {
+
+  static BASE_URL = 'https://gutendex.com/books/' 
+
+  constructor(currentPage=1) {
+    if (instance) throw new Error('book service already instantiated')
+    this.currentPage = currentPage
+  }
+
+  getBooksByPage(){
+    const url = BookService.BASE_URL +'?page=' + this.currentPage
+    return fetch(url)
+    .then(res => res.json())
+    .then(data => data.results)
+    .catch(err => console.error(err))
+  }
+
+  getBookById(id){
+    return fetch(BookService.BASE_URL + id)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => console.error(err))
+  }
+
+
+  getNextPage(){
+    this.currentPage++
+    return this.getBooksByPage()
+  }
+
+  getPrevPage(){
+    this.currentPage --
+    return this.getBooksByPage()
+  }
+
+}
+
+const singletonBookService = new BookService()
+
+export default singletonBookService
